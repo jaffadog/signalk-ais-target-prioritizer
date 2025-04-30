bebfe2f37067f0a84f341f788b534974b03b532b
Jeremy Waters <jaffadog@gmail.com>
2025-04-29 15:38:12 -0930

    wip - shifted to webapp instead of mobile apps

---
+++ scripts/deploy.ps1
@@ -0,0 +1,6 @@
+
+Copy-Item -Force -Path "..\public" -Destination "C:\signalk\signalkhome\.signalk\node_modules\signalk-ais-target-prioritizer\" -Recurse
+Copy-Item -Force -Path "..\index.js" -Destination "C:\signalk\signalkhome\.signalk\node_modules\signalk-ais-target-prioritizer"
+Copy-Item -Force -Path "..\vesper-xb8000-emulator.js" -Destination "C:\signalk\signalkhome\.signalk\node_modules\signalk-ais-target-prioritizer"
+Copy-Item -Force -Path "..\schema.json" -Destination "C:\signalk\signalkhome\.signalk\node_modules\signalk-ais-target-prioritizer"
+Copy-Item -Force -Path "..\package.json" -Destination "C:\signalk\signalkhome\.signalk\node_modules\signalk-ais-target-prioritizer"
